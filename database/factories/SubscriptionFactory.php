0b93b2f8ca4ef7840418cbcaff0fe3882e0d2602
Arnas Kazlauskas <prodigers@gmail.com>
2020-08-31 16:02:01 +0300

    feat: subscription factory

---
+++ database/factories/SubscriptionFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Subscription;
+use Faker\Generator as Faker;
+
+$factory->define(Subscription::class, function (Faker $faker) {
+    return [
+        'active' => $faker->boolean(50),
+        'activeRenewal' => $faker->boolean(50),
+        'expiresAt' => $faker->dateTime,
+    ];
+});
