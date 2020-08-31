494f0b1b5673d1c57bd914bc65bb1fca6713215b
Arnas Kazlauskas <prodigers@gmail.com>
2020-08-31 16:31:48 +0300

    test: AppleCallbackProcessorTest.php subscription not found

--- app/PaymentProcessing/ProviderFactory.php
+++ app/PaymentProcessing/ProviderFactory.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\PaymentProcessing;
 
-use App\PaymentProcessing\Apple\AppleCallbackProcessor;
-
 class ProviderFactory
 {
     /**
--- tests/Unit/App/PaymentProcessing/Apple/AppleCallbackProcessorTest.php
+++ tests/Unit/App/PaymentProcessing/Apple/AppleCallbackProcessorTest.php
@@ -7,21 +7,24 @@ namespace Tests\Unit\App\PaymentProcessing\Apple;
 use App\PaymentProcessing\Apple\AppleCallback;
 use App\PaymentProcessing\Apple\AppleCallbackProcessor;
 use App\PaymentProcessing\Apple\AppleCallbackTransformer;
+use Mockery as m;
 use Tests\TestCase;
 use Prophecy\Argument;
 use Prophecy\Prophecy\ObjectProphecy;
+use App\Subscription;
 
 class AppleCallbackProcessorTest extends TestCase
 {
     private AppleCallbackProcessor $appleCallbackProcessor;
 
     /**
-     * @var ObjectProphecy
+     * @var ObjectProphecy|AppleCallbackTransformer
      */
     private $appleCallbackTransformer;
 
     protected function setUp(): void
     {
+        parent::setUp();
         $this->appleCallbackTransformer = $this->prophesize(AppleCallbackTransformer::class);
         $this->appleCallbackProcessor = new AppleCallbackProcessor(
             $this->appleCallbackTransformer->reveal()
@@ -33,6 +36,20 @@ class AppleCallbackProcessorTest extends TestCase
         $callback = new AppleCallback('unknown', '1');
         $this->appleCallbackTransformer->transform(Argument::any())->willReturn($callback);
         $this->expectExceptionMessage('Unknown apple event');
+
+        $this->appleCallbackProcessor->process([]);
+    }
+
+    public function testThrowsExceptionWhenSubscriptionNotFound(): void
+    {
+        $subscription = m::mock(Subscription::class);
+
+        $this->app->instance(Subscription::class, $subscription);
+
+        $callback = new AppleCallback('REFUND', '1');
+        $this->appleCallbackTransformer->transform(Argument::any())->willReturn($callback);
+
+        $this->expectExceptionMessage('Subscription not found');
 
         $this->appleCallbackProcessor->process([]);
     }
